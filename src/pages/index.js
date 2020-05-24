f45895e9eca54b08ed57d444b9afa51e02707873
eeshamoona <eeshamoona@gmail.com>
2020-05-24 00:20:37 -0700

    Additional style testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,17 @@ import React from "react"
 import { Link } from 'gatsby'
 
 import Layout from "../components/layout"
+import indexStyles from './index.module.scss'
 
 const IndexPage = () => {
 
   return (
     <Layout>
-      <h1>Hello There.</h1>
-      <h2>I'm computer science student at University of Illinois at Urbana-Champaign</h2>
-      <p> Need a software engineer? <Link to="/contactMe">Contact me.</Link></p>
+      <div className={indexStyles.container}>
+        <h1>Hello There.</h1>
+        <h2>I'm computer science student at University of Illinois at Urbana-Champaign</h2>
+        <p> Need a software engineer? <Link to="/contactMe">Contact me.</Link></p>
+      </div>
     </Layout>
   )
 
