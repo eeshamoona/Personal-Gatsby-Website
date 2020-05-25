57fc9b0d9e1fb0750097b88b86f294b4231796c2
eeshamoona <eeshamoona@gmail.com>
2020-05-25 03:41:09 -0700

    Added pages for blog posts

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { graphql, useStaticQuery} from 'gatsby'
+import { Link, graphql, useStaticQuery} from 'gatsby'
 import Layout from "../components/layout"
 
 const BlogPage = () => {
@@ -11,6 +11,9 @@ const BlogPage = () => {
                         frontmatter {
                             title
                             date
+                        }
+                        fields {
+                            slug
                         }
                     }
                 }
@@ -27,7 +30,7 @@ const BlogPage = () => {
                     {data.allMarkdownRemark.edges.map((edge) => {
                             return (
                                 <li>
-                                    <h2>{edge.node.frontmatter.title}</h2>
+                                    <h2><Link to= {`./blog/${edge.node.fields.slug}`}>{edge.node.frontmatter.title}</Link></h2>
                                     <p>{edge.node.frontmatter.date}</p>
                                 </li>
                             )
