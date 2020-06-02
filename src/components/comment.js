797e6e62dea367dcefb700a41ec4d1c2b9010cb5
eeshamoona <eeshamoona@gmail.com>
2020-06-02 09:10:36 -0700

    Adding styling to the comment section

--- src/components/comment.js
+++ src/components/comment.js
@@ -9,7 +9,7 @@ export default class Comments extends React.Component {
                 <div id="commento"/>
                 <Helmet>
                 <script src="https://cdn.commento.io/js/commento.js"
-                data-css-override="./comment.css"
+                data-css-override="https://gist.github.com/eeshamoona/5f8c796033632c08a6634938046dcbba.js"
                 data-auto-init="true">
                 </script>
                 </Helmet>
