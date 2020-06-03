8caa8a059a22a6f7de5e647bec8c0ffafe64bc68
eeshamoona <eeshamoona@gmail.com>
2020-06-02 18:42:46 -0700

    Minor changes

--- src/components/comment.js
+++ src/components/comment.js
@@ -6,12 +6,12 @@ export default class Comments extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <div id="commento"/>
                 <Helmet>
                 <script src="https://cdn.commento.io/js/commento.js"
                 data-css-override="https://gist.githubusercontent.com/eeshamoona/5f8c796033632c08a6634938046dcbba/raw/478fda5532db18a45ba0acb23cf5a4ab05f225d1/comment.css"
                 data-auto-init="true">
                 </script>
+                <div id="commento"/>
                 </Helmet>
             </React.Fragment>
         );
