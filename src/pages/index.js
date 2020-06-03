da2edbf05f0aec53b3e095f35b8cbd4f039b7a16
eeshamoona <eeshamoona@gmail.com>
2020-06-02 19:23:47 -0700

    minor edits

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,8 @@ import Head from '../components/head'
 const IndexPage = () => {
   return (
     <Layout>
-       <link rel="stylesheet" href="/components/comment.css"></link>
-        <Head title = "Home"></Head>
+      <style data-href="/component/comment.css"/>
+          <Head title = "Home"></Head>
         <h1>Hello There.</h1>
         <h2>I'm a computer science student at University of Illinois at Urbana-Champaign</h2>
         <p> Need a software engineer? <Link to="/contactMe">Contact me.</Link></p>
